829dd783c4d80bad610e97e2afd97e8d49863685
Carlos Rojas <carlos.rojas@localhost>
2019-06-18 06:58:11 -0500

    cache first

--- public/sw.js
+++ public/sw.js
@@ -40,3 +40,11 @@ const files = [
         )
     );
   });
+
+  self.addEventListener('fetch', event => {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );  
+  });
