75e81c4f09dcca8822bff9c700789ba1f7603671
Carlos Rojas <carlos.rojas@localhost>
2019-06-19 06:21:39 -0500

    cacheFirst redo

--- public/sw.js
+++ public/sw.js
@@ -42,5 +42,9 @@ const files = [
   });
 
   self.addEventListener('fetch', event => {
-    event.respondWith(caches.match(event.request));
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );  
   });
