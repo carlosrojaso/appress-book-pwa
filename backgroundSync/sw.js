e10b1de0d665d5b5cc880125f12cf23c4c775ef7
Carlos Rojas <carlos.rojas@localhost>
2019-09-10 07:05:13 -0500

    fix(*): cleaning code

--- backgroundSync/sw.js
+++ backgroundSync/sw.js
@@ -1,20 +1,36 @@
 self.addEventListener('sync', (event) => {
-  switch(event.tags) {
-    case 'send-messages':
-      event.waitUnit(
-        sendMessage()
-        .then()
-        .catch(
-          (error) => {
-            if(event.lastChance) {
-              return removeMessage();
-            }
-          }
-        ):
-      );
-    break;
-    default:
-      // code block
+  var url = 'https://jsonplaceholder.typicode.com';
+ 
+  if(event.tags === 'message-queue') {
+    event.waitUntil(() => {
+        return
+          getAllMessagesFrom("message-queue")
+          .then((messages) => {
+            return
+              Promise.all(
+                messages.map(
+                  (message) => {
+                    return fetch(`${url}/posts`,{
+                      method: 'post',
+                      body: JSON.stringify({
+                        title: message.title,
+                        body: message.body,
+                        userId: message.userId
+                      }),
+                      headers: {
+                        "Content-type": "application/json; charset=UTF-8"
+                      }
+                      })
+                      .then(() => {
+                        return deleteMessageFromQueue(item);
+                      });
+                    ;
+                  }
+                );
+              );
+            ;
+          });
+        ;
+      });
   }
-
 });
